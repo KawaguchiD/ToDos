6d11530844a03e0ed8b478d916823ab13edd9a97
KawaguchiD <hirokey.0823@gmail.com>
2019-12-09 01:32:53 +0900

    Not completed

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,11 @@
-function hoge() {
-    console.log("hoge")
+const newtodo = document.querySelector(`.new-todo`)
+const todolist = document.querySelector(`todo-list`)
+
+const createTodo = task => {
+        const html = `
+        <li class="todo-list-item">
+        <span class="todo-list-item-text">${task}</span>
+        <button type="button" onclick="textdelete()" i class="fas fa-trash" id="todo-list-item-trash"></i>
+        </li> `;
+        list.innerHTML+=html;
 }
-hoge();
-const newTodo = document.getElementbyId(`newTodo`)
-console.log(newTodo)
